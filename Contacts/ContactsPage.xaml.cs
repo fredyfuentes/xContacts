c00c3ea9260ee534779e660c11d820ae1e634794
Fredy Ferney Fuentes Beltran <fredy.fuentesb@cecar.edu.co>
2020-03-23 21:17:04 -0500

    Aplicacion de contactos front listo

---
+++ Contacts/ContactsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Contacts
+{
+    public partial class ContactsPage : ContentPage
+    {
+        public ContactsPage()
+        {
+            InitializeComponent();
+        }
+
+        void NewContactToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
